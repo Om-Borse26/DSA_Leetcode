eefa59d5b6e21227933ff8018eb7ee278b24c716
Om Borse <omborse1618@gmail.com>
2025-07-13 16:11:32 +0530

    Time: 6 ms (99.52%) | Memory: 50 MB (81.37%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,44 +1,39 @@
 class Solution {
-    public static int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int MOD = 1000000007;
-        
-        // Arrays to store previous and next smaller elements
-        int[] prevSmaller = new int[n];
-        int[] nextSmaller = new int[n];
-        
-        // Find previous smaller elements
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
+    public int sumSubarrayMins(int[] arr) {
+
+        int len = arr.length, ptr = -1;
+        int[] st = new int[len + 1];
+        long sum = 0; 
+
+        for( int i = 0; i < len; i++ ){
+            
+            while( ptr != -1 && arr[i] < arr[st[ptr]] ){
+                
+                int prevIdx = st[ptr];
+                ptr--;
+                long ele = arr[prevIdx];
+                int prevSmallerIdx = ptr == -1 ? -1 : st[ptr];
+
+                sum  += ele * ( prevIdx - prevSmallerIdx ) * ( i - prevIdx );
+                
             }
-            prevSmaller[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+
+            st[ ++ptr ] = i;
+            
         }
-        
-        // Clear stack for next smaller elements
-        st.clear();
-        
-        // Find next smaller elements
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-                st.pop();
-            }
-            nextSmaller[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
+
+        while( ptr > -1 ){
+
+            int prevIdx = st[ptr];
+            ptr--;
+            long ele = arr[prevIdx];
+            int prevSmallerIdx = ptr == -1 ? -1 : st[ptr]; 
+            
+            sum +=  ele * ( prevIdx - prevSmallerIdx ) * ( len - prevIdx );
+            
         }
+
+        return (int) (sum % 1_000_000_007);
         
-        long result = 0;
-        
-        // Calculate contribution of each element
-        for (int i = 0; i < n; i++) {
-            long left = i - prevSmaller[i];  // Number of subarrays where arr[i] is minimum on the left
-            long right = nextSmaller[i] - i; // Number of subarrays where arr[i] is minimum on the right
-            long contribution = (left * right * arr[i]) % MOD;
-            result = (result + contribution) % MOD;
-        }
-        
-        return (int) result;
     }
 }
