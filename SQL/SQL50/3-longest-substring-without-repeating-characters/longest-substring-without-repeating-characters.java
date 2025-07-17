4e5b73fb9d0c844348141c598b36f640cc9aa794
Om Borse <omborse1618@gmail.com>
2025-07-17 12:15:42 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (95.63%) - LeetSync

--- SQL/SQL50/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ SQL/SQL50/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,23 +1,37 @@
 class Solution {
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s.length() <= 1) return s.length();
+        
+    //     int maxcount = 0;
+    //     int p1 = 0;
+        
+    //     while (p1 < s.length()) {
+    //         boolean[] seen = new boolean[128]; 
+    //         int p2 = p1;
+            
+    //         while (p2 < s.length() && !seen[s.charAt(p2)]) {
+    //             seen[s.charAt(p2)] = true;
+    //             p2++;
+    //         }
+            
+    //         maxcount = Math.max(maxcount, p2 - p1);
+    //         p1++; 
+    //     }
+        
+    //     return maxcount;
+    // }
+
+
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() <= 1) return s.length();
-        
-        int maxcount = 0;
-        int p1 = 0;
-        
-        while (p1 < s.length()) {
-            boolean[] seen = new boolean[128]; 
-            int p2 = p1;
-            
-            while (p2 < s.length() && !seen[s.charAt(p2)]) {
-                seen[s.charAt(p2)] = true;
-                p2++;
-            }
-            
-            maxcount = Math.max(maxcount, p2 - p1);
-            p1++; 
+        int n = s.length();
+        int max = 0;
+        int left = 0;
+        int[] last = new int[128];
+        for (int r = 0; r < n; r++) {
+            left = Math.max(left, last[s.charAt(r)]);
+            max = Math.max(max, r - left + 1);
+            last[s.charAt(r)] = r + 1;
         }
-        
-        return maxcount;
+        return max;
     }
 }
