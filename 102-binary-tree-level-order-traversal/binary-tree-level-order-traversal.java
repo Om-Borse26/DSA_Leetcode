f6f6da1a7fa00520cad25d5f13a98f630acef9d5
Om Borse <omborse1618@gmail.com>
2025-07-22 13:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (53.46%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,46 +15,61 @@
  */
 class Solution {
 
-    public static void helper(TreeNode root, List<List<Integer>> l1){
+    // public static void helper(TreeNode root, List<List<Integer>> l1){
 
-        if(root == null){
-            return;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
+    //     if(root == null){
+    //         return;
+    //     }
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root);
+    //     q.add(null);
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0;i<size;i++){
-            TreeNode currentNode = q.remove();
-            if(currentNode == null){
-                if(q.isEmpty()){
-                    break;
-                }
-                else{
-                    q.add(null);
-                }
-            }
-            else{
-                temp.add(currentNode.val);
-                if(currentNode.left != null){
-                    q.add(currentNode.left);
-                }
-                if(currentNode.right != null){
-                    q.add(currentNode.right);
-                }
-            }
-            }
+    //     while(!q.isEmpty()){
+    //         int size = q.size();
+    //         List<Integer> temp = new ArrayList<>();
+    //         for(int i=0;i<size;i++){
+    //         TreeNode currentNode = q.remove();
+    //         if(currentNode == null){
+    //             if(q.isEmpty()){
+    //                 break;
+    //             }
+    //             else{
+    //                 q.add(null);
+    //             }
+    //         }
+    //         else{
+    //             temp.add(currentNode.val);
+    //             if(currentNode.left != null){
+    //                 q.add(currentNode.left);
+    //             }
+    //             if(currentNode.right != null){
+    //                 q.add(currentNode.right);
+    //             }
+    //         }
+    //         }
             
-            l1.add(temp);
-        }
-    }
+    //         l1.add(temp);
+    //     }
+    // }
 
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> l1 = new ArrayList<>();
+    //     helper(root, l1);
+    //     return l1;
+    // }
+
+    List<List<Integer>> levels=new ArrayList<List<Integer>>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> l1 = new ArrayList<>();
-        helper(root, l1);
-        return l1;
+        if(root==null) return levels;
+        helper(root, 0);
+        return levels;    
+    }
+    public void helper(TreeNode node, int level){
+        if(levels.size()==level){
+            levels.add(new ArrayList<Integer>());
+        }
+        levels.get(level).add(node.val);
+        if(node.left!=null) helper(node.left,level+1);
+        if(node.right!=null)helper(node.right,level+1);
     }
 }
