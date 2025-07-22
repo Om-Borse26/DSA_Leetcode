2a3c00d677aad28fa1fe51f81a5258d2c06a5d5f
Om Borse <omborse1618@gmail.com>
2025-07-22 13:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (27.07%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,21 +15,42 @@
  */
 class Solution {
 
-    public static void helper(TreeNode root, List<Integer> l1){
-        if(root == null){
-            return;
-        }
+    // public static void helper(TreeNode root, List<Integer> l1){
+    //     if(root == null){
+    //         return;
+    //     }
 
-        l1.add(root.val);
-        helper(root.left, l1);
-        helper(root.right,l1);
-    }
+    //     l1.add(root.val);
+    //     helper(root.left, l1);
+    //     helper(root.right,l1);
+    // }
+
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> l1 = new ArrayList<>();
+    //     helper(root, l1);
+
+    //     return l1;
+        
+    // }
 
     public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
         List<Integer> l1 = new ArrayList<>();
-        helper(root, l1);
 
+        if(root == null){
+            return l1;
+        }
+        s.push(root);
+        while(!s.isEmpty()){
+            root = s.pop();
+            l1.add(root.val);
+            if(root.right != null){
+                s.push(root.right);
+            }
+            if(root.left != null){
+                s.push(root.left);
+            }
+        }
         return l1;
-        
     }
 }
