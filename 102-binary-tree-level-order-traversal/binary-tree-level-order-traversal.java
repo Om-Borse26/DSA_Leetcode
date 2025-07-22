0cb048028f69718e747c3514c167abccf56cbb12
Om Borse <omborse1618@gmail.com>
2025-07-22 13:05:32 +0530

    Time: 1 ms (91.21%) | Memory: 45 MB (53.46%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public static void helper(TreeNode root, List<List<Integer>> l1){
+
+        if(root == null){
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<size;i++){
+            TreeNode currentNode = q.remove();
+            if(currentNode == null){
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                temp.add(currentNode.val);
+                if(currentNode.left != null){
+                    q.add(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    q.add(currentNode.right);
+                }
+            }
+            }
+            
+            l1.add(temp);
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> l1 = new ArrayList<>();
+        helper(root, l1);
+        return l1;
+    }
+}
