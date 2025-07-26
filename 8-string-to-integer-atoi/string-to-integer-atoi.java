64831fd9288bfa1c94cbe603f06e76d50009b0f2
Om Borse <omborse1618@gmail.com>
2025-07-26 16:53:42 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (80.23%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,31 +1,71 @@
 class Solution {
+
+    public static int helper(String s,int i, int sign, int result, boolean started){
+
+        if (i == s.length()) {
+            return sign * result;
+        }
+        
+        char ch = s.charAt(i);
+
+        if(!started){
+        if(ch == ' '){
+            return helper(s,i+1,1,0,false);
+        }
+        if(ch == '+'){
+            return helper(s,i+1,1,result,true);
+        }
+        if(ch == '-'){
+            return helper(s,i+1,-1,result,true);
+        }
+        if(Character.isDigit(ch)){
+            return helper(s,i+1,sign,ch - '0',true);
+        }
+        } else {
+            if (Character.isDigit(ch)) {
+                int digit = ch - '0';
+                if (result > (Integer.MAX_VALUE - digit) / 10) {
+                    return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                int newResult = result * 10 + digit;
+                return helper(s, i + 1, sign, newResult, true);
+            } 
+        }
+    return sign * result;
+}
+
+
     public int myAtoi(String s) {
-        int i = 0;
-        int n = s.length();
-        int sign = 1;
 
-        s = s.trim();
+        return helper(s,0,1,0,false);
+        // int i = 0;
+        // int n = s.length();
+        // int sign = 1;
 
-        if (n > 0 && i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
-            if (s.charAt(i) == '-') {
-                sign = -1;
-            }
-            i++;
-        }
+        // s = s.trim();
 
-        int num = 0;
+        // if (n > 0 && i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+        //     if (s.charAt(i) == '-') {
+        //         sign = -1;
+        //     }
+        //     i++;
+        // }
 
-        while (i < s.length() && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
+        // int num = 0;
 
-            if (num > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
+        // while (i < s.length() && Character.isDigit(s.charAt(i))) {
+        //     int digit = s.charAt(i) - '0';
 
-            num = num * 10 + digit;
-            i++;
-        }
+        //     if (num > (Integer.MAX_VALUE - digit) / 10) {
+        //         return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        //     }
 
-        return num * sign;
+        //     num = num * 10 + digit;
+        //     i++;
+        // }
+
+        // return num * sign;
+
+
     }
 }
